a2dfcfccaa515c6772cb4290b343566ca0c3ab40
Hugo Aboud <hugoaboud@gmail.com>
2025-08-25 04:46:05 -0300

    graph wip

--- client-vue/src/components/Graph.vue
+++ client-vue/src/components/Graph.vue
@@ -11,56 +11,92 @@ const graph = useTemplateRef<HTMLDivElement>('graph');
 const app = useAppStore();
 
 onMounted(() => {
-    var cy = cytoscape({
-        container: graph.value,
+    
 
-        elements: [ // list of graph elements to start with
-            { // node a
-            data: { id: 'a' }
-            },
-            { // node b
-            data: { id: 'b' }
-            },
-            { // edge ab
-            data: { id: 'ab', source: 'a', target: 'b' }
-            }
-        ],
+    // var cy = cytoscape({
+    //     container: graph.value,
 
-        style: [ // the stylesheet for the graph
-            {
-            selector: 'node',
-            style: {
-                'background-color': '#666',
-                'label': 'data(id)'
-            }
-            },
+    //     elements: [ // list of graph elements to start with
+    //         { // node a
+    //         data: { id: 'a' }
+    //         },
+    //         { // node b
+    //         data: { id: 'b' }
+    //         },
+    //         { // edge ab
+    //         data: { id: 'ab', source: 'a', target: 'b' }
+    //         }
+    //     ],
 
-            {
-            selector: 'edge',
-            style: {
-                'width': 3,
-                'line-color': '#ccc',
-                'target-arrow-color': '#ccc',
-                'target-arrow-shape': 'triangle',
-                'curve-style': 'bezier'
-            }
-            }
-        ],
+    //     style: [ // the stylesheet for the graph
+    //         {
+    //         selector: 'node',
+    //         style: {
+    //             'background-color': '#666',
+    //             'label': 'data(id)'
+    //         }
+    //         },
 
-        layout: {
-            name: 'grid',
-            rows: 1
-        }
-    });
+    //         {
+    //         selector: 'edge',
+    //         style: {
+    //             'width': 3,
+    //             'line-color': '#ccc',
+    //             'target-arrow-color': '#ccc',
+    //             'target-arrow-shape': 'triangle',
+    //             'curve-style': 'bezier'
+    //         }
+    //         }
+    //     ],
+
+    //     layout: {
+    //         name: 'grid',
+    //         rows: 1
+    //     }
+    // });
     
     fetch();
 })
 
 async function fetch() {
-    // await app.daemon?.trx('core').run(async trx => {
-    //     const deviceTypes = await trx.bucket('device_type').readAll();
-    //     console.log(deviceTypes);
-    // })
+    const nodes = await app.run('core', trx => trx.bucket('node').readAll());
+    const links = await app.run('core', trx => trx.bucket('link').readAll());
+    
+    var cy = cytoscape({
+        container: graph.value,
+        elements: [
+            ...nodes.map(node => ({
+                data: { id: node.id }
+            })),
+            ...links.map(link => ({
+                data: { id: link.id, source: link.from_node_id, target: link.to_node_id }
+            })),
+        ],
+        style: [ // the stylesheet for the graph
+            {
+                selector: 'node',
+                style: {
+                    'color': '#ddd',
+                    'background-color': '#666',
+                    'label': 'data(id)'
+                }
+            },
+            {
+                selector: 'edge',
+                style: {
+                    'width': 3,
+                    'line-color': '#ccc',
+                    'target-arrow-color': '#ccc',
+                    'target-arrow-shape': 'triangle',
+                    'curve-style': 'bezier'
+                }
+            }
+        ],
+        layout: {
+            name: 'grid',
+            rows: 1
+        }
+    });
 }
 
 </script>
